b649940feb7ba291a2af2f1886dccdc84549bb19
Rohit Shinde <rohit@honestbank.com>
2023-04-04 15:43:53 +0700

    lint

--- policies/constant_test.go
+++ policies/constant_test.go
@@ -11,14 +11,14 @@ import (
 
 func TestGetConstantPolicy(t *testing.T) {
 	t.Run("returns correct duration based on count", func(t *testing.T) {
-		policy := policies.GetConstantPolicy(time.Millisecond*100)
+		policy := policies.GetConstantPolicy(time.Millisecond * 100)
 		assert.Equal(t, time.Millisecond*0, policy(0))
 		assert.Equal(t, time.Millisecond*100, policy(1))
 		assert.Equal(t, time.Millisecond*100, policy(2))
 		assert.Equal(t, time.Millisecond*100, policy(3))
 	})
 	t.Run("value is capped at max count", func(t *testing.T) {
-		policy := policies.GetConstantPolicy(time.Millisecond*100)
+		policy := policies.GetConstantPolicy(time.Millisecond * 100)
 		assert.Equal(t, policy(10), policy(10))
 		assert.Equal(t, policy(10), policy(11))
 		assert.Equal(t, policy(10), policy(12))
